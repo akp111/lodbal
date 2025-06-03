10cb917a892de3126277d9575eea397f4afe621a
Ashis <pradhanashish95@gmail.com>
2025-06-04 00:58:12 +0530

    improved example

--- main.go
+++ main.go
@@ -4,12 +4,6 @@ import (
 	"github.com/akp111/lodbal/lodbal"
 )
 
-type DummyLoadBalAlgo struct {
-}
-
-func (lba DummyLoadBalAlgo) LbAlgo(servers []loadbal.ServerConfig) (loadbal.ServerConfig, error) {
-	return servers[0], nil
-}
 
 func main() {
 	server1 := loadbal.CreateServerConfig("http://localhost:3000", 0, "http://localhost:3000/health", 10)
@@ -17,7 +11,5 @@ func main() {
 	var servers []*loadbal.ServerConfig;
 	servers = append(servers, &server1)
 	servers = append(servers, &server2)
-	// Initialize server map - safe in single-threaded main function
-	// dummyLbAlgo := DummyLoadBalAlgo{}
 	loadbal.CreateLodBal(servers, loadbal.RoundRobinLoadBalancerAlgo{}, 100, 8000)
 }
